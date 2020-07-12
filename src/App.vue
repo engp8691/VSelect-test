0084e020686560b9da50d1d1e0e94e90f72b8c65
Yonglin Li <yonglin.li@gmail.com>
2020-07-11 23:58:38 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <InfiniteScroll :allOptions="countries" :selectedValue="1916" placeHolder="Year" />
+    <InfiniteScroll :allOptions="months" selectedValue="July" placeHolder="Month" />
+    <InfiniteScroll :allOptions="days" :selectedValue="23" placeHolder="Day" />
+  </div>
+</template>
+
+<script>
+import InfiniteScroll from './components/InfiniteScroll.vue';
+
+export default {
+  computed: {
+    countries(){
+      const years = [];
+      for(let i=2020; i>1900; i--){
+        years.push(`${i}`);
+      }      
+      return years;
+    },
+    months(){
+      return [
+        "January",
+        "Febuary",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "Octable",
+        "November",
+        "December",
+      ];
+    },
+    days(){
+      const days = [];
+      for(let i=1; i<32; i++){
+        days.push(`${i}`);
+      }      
+      return days;
+    }
+  },
+  name: 'App',
+  components: {
+    InfiniteScroll
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
